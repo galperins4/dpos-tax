896a5a3c0b39ba9bc92e4c62a0f3b8d425ccd194
galperins4 <galperins4@gmail.com>
2018-07-30 23:14:38 -0500

    Add files via upload

--- dpos_tax.py
+++ dpos_tax.py
@@ -6,13 +6,15 @@ test_acct = "AMpPxXJZ7qdLbNUrVQV82ozDF2UZgHGB5L"
 
 def buy(acct):
     s = "buy"
-    testing = taxdb.get_transactions(acct, s)
-    print(testing)
-
+    return taxdb.get_transactions(acct, s)
 
 def sell(acct):
     s = "sell"
-
+    return taxdb.get_transactions(acct, s)
 
 if __name__ == '__main__':
-    buy(test_acct)
+    buys = buy(test_acct)
+    sells = sell(test_acct)
+
+    print("buys", buys)
+    print("sells", sells)
