5614699c1439dd7c6c71ad2de57ffd91c21fedde
galperins4 <galperins4@gmail.com>
2018-07-30 23:05:50 -0500

    Add files via upload

--- dpos_tax.py
+++ dpos_tax.py
@@ -3,14 +3,16 @@ from tax_db import TaxDB
 taxdb = TaxDB('db', 'username', 'db_pw')
 test_acct = "AMpPxXJZ7qdLbNUrVQV82ozDF2UZgHGB5L"
 
-buy(test_acct)
 
 def buy(acct):
     s = "buy"
     testing = taxdb.get_transactions(acct, s)
     print(testing)
 
+
 def sell(acct):
     s = "sell"
 
 
+if __name__ == '__main__':
+    buy(test_acct)
