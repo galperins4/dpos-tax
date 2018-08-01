10f781ba89649fafe424547a2620530efd414d17
galperins4 <galperins4@gmail.com>
2018-07-31 21:27:04 -0500

    Add files via upload

--- dpos_tax.py
+++ dpos_tax.py
@@ -1,18 +1,20 @@
 from tax_db import TaxDB
 import requests
+import time
+import datetime
 
 
-taxdb = TaxDB('db', 'username', 'db_pw')
+taxdb = TaxDB('ark_mainnet', 'username', 'password')
 
 test_acct = "AMpPxXJZ7qdLbNUrVQV82ozDF2UZgHGB5L"
 epoch = 1490101200
 atomic = 100000000
+tax_rate = 0.24
 
-def get_market_price(ts,v):
+def get_market_price(ts):
     url = 'https://min-api.cryptocompare.com/data/pricehistorical'
     fsym = 'ARK'
     tsyms = 'USD'
-
     adj_ts = ts+epoch
 
     # set request params
@@ -21,32 +23,40 @@ def get_market_price(ts,v):
               "ts": adj_ts}
 
     r = requests.get(url, params=params)
-    price = r.json()['ARK']['USD']
-    tax_price = v * price
+    time.sleep(0.25)
+    return r.json()['ARK']['USD']
 
-    return tax_price
-
-def buy(acct):
-    buy_orders = []
+def create_buy_records(b):
+    orders = []
     counter = 1
-    s = "buy"
-    buys = taxdb.get_transactions(acct, s)
 
-    for i in buys:
+    for i in b:
         # add attributes timestamp, total amount, tax lot
         ts = i[0]
-        total_amt = (i[1]+i[2])/atomic
+        # don't include fee in incoming records
+        order_amt = i[1] / atomic
         tax_lot = counter
-        market_value = get_market_price(ts, total_amt)
+        price = get_market_price(ts)
+        market_value = price * total_amount
+        convert_ts = convert_timestamp((ts+epoch))
+        withold = market_value * tax_rate
 
         # create order record including
-        t = [ts, total_amt, tax_lot, total_amt, market_value]
+        t = [tax_lot, ts, order_amt, price, market_value, withold, convert_ts]
 
         # append to buy_orders
-        buy_orders.append(t)
+        orders.append(t)
 
         # increment counter
-        counter +=1
+        counter += 1
+
+    return orders
+
+def buy(acct):
+
+    s = "buy"
+    buys = taxdb.get_transactions(acct, s)
+    buy_orders = create_buy_records(buys)
 
     return buy_orders
 
@@ -54,8 +64,12 @@ def sell(acct):
     s = "sell"
     return taxdb.get_transactions(acct, s)
 
+def convert_timestamp(ts):
+    return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
 if __name__ == '__main__':
     buys = buy(test_acct)
     sells = sell(test_acct)
 
-    print("buys", buys)
+    for i in buys:
+        print(i)
