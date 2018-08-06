a986ed7cc0c311a13433dd839432af7ce3543677
galperins4 <galperins4@gmail.com>
2018-08-05 23:24:29 -0500

    Add files via upload

--- core/price.py
+++ core/price.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 import requests
 
-class Conversion:
-    def __init__(self, a="ARK", b="KAPU"):
-        self.a = self.testing(a)
-        self.b = self.testing(b)
+class Price:
+    def __init__(self):
+        self.fsym = "ARK'
+        self.tsyms = 'USD,EUR'
+        self.url = 'https://min-api.cryptocompare.com/data/pricehistorical'
+
+    def get_market_price(ts):
+        # set request params
+        params = {"fsym": self.fsym,
+                  "tsyms": self.tsyms,
+                  "ts": ts}
+
+        r = requests.get(self.url, params=params)
+
+        output = [ts, r.json()['ARK']['USD'], r.json()['ARK']['EUR']]
+
+        return output
