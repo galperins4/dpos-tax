9ffe98dee82948b6ecfe19688213e3239648009c
galperins4 <galperins4@gmail.com>
2018-08-05 22:10:15 -0500

    Add files via upload

--- core/taxdb.py
+++ core/taxdb.py
@@ -1,14 +1,90 @@
 #!/usr/bin/env python
-import psycopg2
+import sqlite3
+import threading
 
-class DB:
-    def __init__(self, db, u, pw):
-        self.connection = psycopg2.connect(
-            dbname = db,
-            user = u,
-            password= pw,
-            host='localhost',
-            port='5432'
-        )
-        
-        self.cursor=self.connection.cursor()
+lock = threading.Lock()
+
+
+class TaxDB:
+    def __init__(self, u):
+        self.connection = sqlite3.connect('/home/' + u + '/dpos-tax/tax.db', check_same_thread=False)
+        self.cursor = self.connection.cursor()
+
+    def commit(self):
+        return self.connection.commit()
+
+    def execute(self, query, args=[]):
+        try:
+            lock.acquire(True)
+            res = self.cursor.execute(query, args)
+        finally:
+            lock.release()
+
+        return res
+
+    def executemany(self, query, args):
+        try:
+            lock.acquire(True)
+            res = self.cursor.executemany(query, args)
+        finally:
+            lock.release()
+
+        return res
+
+    def fetchone(self):
+        return self.cursor.fetchone()
+
+    def fetchall(self):
+        return self.cursor.fetchall()
+
+    def setup(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS ark_prices (timestamp int, usd float, eur float )")
+
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS delegates (address varchar(64) )")
+
+        self.connection.commit()
+
+
+    def update_prices(self, prices):
+        newPrices=[]
+
+        for p in prices:
+            self.cursor.execute("SELECT timestamp FROM prices WHERE timestamp = ?", (p[0],))
+
+            if self.cursor.fetchone() is None:
+                newPrices.append((p[0], p[1], p[2]))
+
+        self.executemany("INSERT INTO delegates VALUES (?,?,?)", newPrices)
+
+        self.commit()
+
+
+    def get_prices(self):
+        return self.cursor.execute("SELECT * FROM prices")
+
+
+    def single_price(self, ts):
+        return self.cursor.execute("SELECT * FROM prices WHERE timestamp = '{ts}'")
+
+
+    def update_delegates(self, delegates):
+        newDelegates=[]
+
+        for d in delegates:
+            self.cursor.execute("SELECT address FROM delegates WHERE address = ?", (d[0],))
+
+            if self.cursor.fetchone() is None:
+                newDelegates.append((d[0]))
+
+        self.executemany("INSERT INTO delegates VALUES (?)", newDelegates)
+
+        self.commit()
+
+    def get_delegates(self):
+        return self.cursor.execute("SELECT * FROM delegates")
+
+
+    def single_delegate(self, addr):
+        return self.cursor.execute("SELECT * FROM delegates WHERE address = '{addr}'")
--- dpos_tax.py
+++ dpos_tax.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, render_template
-from tax_db import TaxDB
+from core.psql import TaxDB
 import csv
 import requests
 import time
