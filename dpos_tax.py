bff1e225a9b5158dd3b78d4529245fb61a461e72
galperins4 <galperins4@gmail.com>
2018-08-13 19:39:59 -0700

    Add files via upload

--- dpos_tax.py
+++ dpos_tax.py
@@ -38,19 +38,10 @@ def get_db_price(ts):
         if i[0] >= ts:
             break
 
-    print("counter", counter)
-    print("price", p[counter + 1][1])
-    # price = p[counter + 1][1]
-
-    '''
-    # check to see if tx is after last timestamp and assign last
-    if counter+1 > len(p):
+    if (counter+1) == len(p):
         price = p[counter][1]
     else:
         price = p[counter + 1][1]
-
-    '''
-    #price = 1
 
     return price
 
